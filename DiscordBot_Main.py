19e10e40f47387a7a461a7f098065fdc4f109320
wils <79368146+WilsGao@users.noreply.github.com>
2024-04-08 23:13:34 +0800

    Update DiscordBot_Main.py

--- DiscordBot_Main.py
+++ DiscordBot_Main.py
@@ -9,8 +9,9 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.members = True
 
-intents = discord.Intents.default()# 啟用發送/接收訊息的權限
-intents.messages = True# 如果你的機器人需要與伺服器交互，也需要啟用此權限
+intents = discord.Intents.default()
+intents.messages = True # 啟用發送/接收訊息的權限
+intents.guilds = True # 如果你的機器人需要與伺服器交互，也需要啟用此權限
 
 # 初始化機器人客戶端
 bot = commands.Bot(command_prefix='!', intents=intents)
